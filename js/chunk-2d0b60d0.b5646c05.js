defc0dcf5b9aaa5438a14755e06152851bc3a1c7
Rizky Rosyid Hidayat <rizky.hidayat@students.amikom.ac.id>
2020-06-07 09:43:24 +0700

    consume api response

---
+++ js/chunk-2d0b60d0.b5646c05.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b60d0"],{"1c08":function(e,n,t){"use strict";t.r(n);var s=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"pa-md-6 pa-sm-3"},[t("router-view")],1)},a=[],c=t("2877"),u={},l=Object(c["a"])(u,s,a,!1,null,null,null);n["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d0b60d0.b5646c05.js.map
